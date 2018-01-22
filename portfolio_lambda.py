e399d9f2ebc4ba60a65fd74ea5957c23016a6111
kpouliasis <konstantinos.pouliasis@gmail.com>
2018-01-22 14:17:12 -0500

    adding lambda function

--- portfolio_lambda.py
+++ portfolio_lambda.py
@@ -5,9 +5,10 @@ def lambda_handler(event, context):
     import boto3
     from botocore.client import Config
     S3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:582479462404:deployBio')
 
-
-
+    try: 
     IN_BUCKET = S3.Bucket('biobuild.pouliasis.io')
     OUT_BUCKET = S3.Bucket('bio.pouliasis.io')
     PORTFOLIO_ZIP = StringIO.StringIO()
@@ -19,4 +20,5 @@ def lambda_handler(event, context):
             obj = myzip.open(nm)
             OUT_BUCKET.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
             OUT_BUCKET.Object(nm).Acl().put(ACL='public-read')
+    topic.publish(TopicArn = 'arn:aws:sns:us-east-1:582479462404:deployBio', TopicSubject="Testing", Message= "from lambda")
     return 'Hello from Lambda'
